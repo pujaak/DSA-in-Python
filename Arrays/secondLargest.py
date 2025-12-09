78b5cd0ee7bcbab092a0b86ef53ed40b8f84b0f2
Puja <itispujakumari21@gmail.com>
2025-12-09 08:10:16 +0530

    Implement second method to find the second largest element in a list

--- Arrays/secondLargest.py
+++ Arrays/secondLargest.py
@@ -1,29 +1,44 @@
-def secondLargestElement(nums):
-    max = nums[0]
-    for i in nums:
-        if i > max:
-            max = i
+# def secondLargestElement(nums):
+#     print(f"1st menthod, len of nums: {len(nums)}")
+#     max = nums[0]
+#     for i in nums:
+#         if i > max:
+#             max = i
 
-    # print(f"Max: {max}")
-    while max in nums:
-        nums.remove(max)
-    # print(nums)
+#     # print(f"Max: {max}")
+#     while max in nums:
+#         nums.remove(max) # this modifies the original list
+#     # print(nums)
 
-    if len(nums) == 0:
-        return -1
-    else:
-        max2 = nums[0]
-        for i in nums:
-            if i > max2:
-                max2 = i
-        # print(f"2nd Max: {max2}")
-        return max2
+#     if len(nums) == 0:
+#         return -1
+#     else:
+#         max2 = nums[0]
+#         for i in nums:
+#             if i > max2:
+#                 max2 = i
+#         # print(f"2nd Max: {max2}")
+#         return max2
     
-# nums = [8, 8, 7, 6, 5]
-# nums = [10, 10, 10, 10, 10]
-nums = [7, 7, 2, 2, 10, 10, 10]
-print(secondLargestElement(nums))
-        
 
-                
-        
+
+# second way
+def secondLargestElement2(nums):
+    largest = slargest = -1
+    
+    for i in range(0, len(nums)):
+        if nums[i] > largest : 
+            largest = nums[i]
+            
+    for i in range(0,len(nums)):
+        if nums[i] > slargest and nums[i] != largest:
+            slargest = nums[i]
+    return slargest
+
+
+nums = [8, 8, 7, 6, 5]
+# nums = [10, 10, 10, 10, 10]
+# nums = [7, 7, 2, 2, 10, 10, 10]
+# nums = [1, 2, 3, 1, 2, 1]
+# print(secondLargestElement(nums))
+print(secondLargestElement2(nums))
